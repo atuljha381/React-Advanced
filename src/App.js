2fce71da44f46143f84ea1b392de5afbb2bd7e07
atuljha381 <mail.atuljha@gmail.com>
2023-01-21 11:48:41 +0530

    Changes needed from other machine

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
+import { useEffect } from 'react';
 import {useSelector} from 'react-redux'
 
 function App() {
   const cartShow = useSelector(state => state.ui.cartIsVisible)
+  const cart = useSelector(state => state.cart)
+
+  useEffect( ()=> {
+    fetch('https://redux-advanced-c3df3-default-rtdb.firebaseio.com/cart.json',
+    {
+      method:'PUT',
+      body:JSON.stringify(cart)
+    })
+  }, [cart])
+
   return (
     <Layout>
       {cartShow && <Cart />}
